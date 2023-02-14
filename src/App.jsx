b8c749dc3c46598507d8a13fb2c6bf1cb426c7ba
Blasty <NightLabMC@gmail.com>
2023-02-14 10:44:44 +0100

    commit shifumi

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,49 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import React, { useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [userChoice, setUserChoice] = useState("");
+  const [computerChoice, setComputerChoice] = useState("");
+  const [result, setResult] = useState("");
+
+  const choices = ["pierre", "papier", "ciseaux"];
+
+  const handleClick = (choice) => {
+    const randomNum = Math.floor(Math.random() * 3);
+    const computerChoice = choices[randomNum];
+
+    setUserChoice(choice);
+    setComputerChoice(computerChoice);
+
+    if (choice === computerChoice) {
+      setResult("Egalité !");
+    } else if (
+      (choice === "pierre" && computerChoice === "ciseaux") ||
+      (choice === "papier" && computerChoice === "pierre") ||
+      (choice === "ciseaux" && computerChoice === "papier")
+    ) {
+      setResult("Vous avez gagné !");
+    } else {
+      setResult("Vous avez perdu !");
+    }
+  };
+
+  const backgroundColor = result === "Vous avez gagné !" ? "green" : result === "Vous avez perdu !" ? "red" : "yellow";
 
   return (
-    <div className="App">
+    <div style={{ display: "flex", justifyContent: "center", alignItems: "center", height: "100vh", backgroundColor }}>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h1>Pierre-Papier-Ciseaux</h1>
+        <div>
+          <button onClick={() => handleClick("pierre")}>Pierre</button>
+          <button onClick={() => handleClick("papier")}>Papier</button>
+          <button onClick={() => handleClick("ciseaux")}>Ciseaux</button>
+        </div>
+        <h2>Votre choix: {userChoice}</h2>
+        <h2>Choix de l'ordinateur: {computerChoice}</h2>
+        <h1>{result}</h1>
       </div>
-      <h1>Vite + dsdsdsdcsdsReact</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
